6a2024c0d5dc3540ff957a9372a7ec19d26cd76f
anaelisa8 <anaelisaestrada@gmail.com>
2023-09-11 12:04:30 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import "./App.css"
+import Navbar from "./components/NavBar.js";
 
-function App() {
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Raleway', 
+      '-apple-system',
+      'BlinkMacSystemFont',
+      "'Lato', sans-serif",
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    color: '#fff'
+  }
+});
+
+const App= () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router >
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+        theme="dark"
+      />
+      <ThemeProvider theme={theme}>
+        <Navbar></Navbar>
+        <div className="App">
+          <Routes>
+            <Route path="/"/>
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
+};
 
 export default App;
